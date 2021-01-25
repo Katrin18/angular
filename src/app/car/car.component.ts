0266ac322b38a8fd3c9dab07e230437bcba9f695
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2021-01-26 00:32:50 +0200

    les-1

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,18 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+   @Input()
+  car:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
