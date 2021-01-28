9b223354d7d7074eee819858b30de1f24c31bf26
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2021-01-28 21:23:18 +0200

    les-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import { HomeComponent } from './components/home/home.component';
+import { FullCarComponent } from './components/full-car/full-car.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    PostsComponent,
+    CarsComponent,
+    CarComponent,
+    HomeComponent,
+    FullCarComponent,
+
   ],
   imports: [
     BrowserModule,
-HttpClientModule
+RouterModule.forRoot([
+  {
+    path:'link/home',component:HomeComponent
+  },
+  {
+    path:'link/cars',component:CarsComponent,children:[
+      {
+        path:':id',component:FullCarComponent
+      }
+    ]
+  }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
